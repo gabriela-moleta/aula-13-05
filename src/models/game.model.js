4d0c5d9eac5fcfadfd62747504e4b75bcd24ae71
Gabriela Moleta <gabriela.moleta@aluno.senai.br>
2025-05-13 11:48:05 -0300

    feat: create records

--- src/models/game.model.js
+++ src/models/game.model.js
@@ -4,12 +4,22 @@ class GameModel {
     //Obter todos os jogos
     async findAll() {
         const games = await prisma.game.findMany();
-        
+
         return {
             total: games.length,
             games,
         };
     }
+
+//Criar um novo jogo
+async create(data) {
+    const game = await prisma.game.create({
+        data,
+    });
+
+    return game;
 }
+}
+
 
 export default new GameModel();
